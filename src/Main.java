45a24f6af10f06c1184e10555e23906b2c92a4e9
sdzyryan <sdzyryan@gmail.com>
2023-04-03 19:01:52 +0500

    task 5-7

--- src/Main.java
+++ src/Main.java
@@ -40,17 +40,23 @@ public class Main {
 
     public static void task5 () {
         System.out.println("Задача 5");
-
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным.");
+        }
     }
 
     public static void task6 () {
         System.out.println("Задача 6");
-
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
     }
 
     public static void task7 () {
         System.out.println("Задача 7");
-
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
     }
 
     public static void task8 () {
