2976d23ddcfa9c3707fe200a2edcfd72389f5c0a
sdzyryan <sdzyryan@gmail.com>
2023-04-03 18:55:30 +0500

    task 1-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        for (int i = 1; i<=10; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task3 () {
+        System.out.println("Задача 3");
+        for (int i = 0; i < 17; i=i+2) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task4 () {
+        System.out.println("Задача 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task5 () {
+        System.out.println("Задача 5");
+
+    }
+
+    public static void task6 () {
+        System.out.println("Задача 6");
+
+    }
+
+    public static void task7 () {
+        System.out.println("Задача 7");
+
+    }
+
+    public static void task8 () {
+        System.out.println("Задача 8");
+
     }
 }
